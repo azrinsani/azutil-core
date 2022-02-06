0fec9a4c25a5f86ea36e985b15bd386de3514173
Azrin Sani <azrinsani@gmail.com>
2022-02-06 16:33:59 +0800

    Update AzExtensions.cs
    Fixed Remove Where

--- AzExtensions.cs
+++ AzExtensions.cs
@@ -46,7 +46,6 @@ namespace azutil_core
             return null;
         }
 
-
         public static ICollection<T> RemoveWhere<T>(this ICollection<T> col, Func<T, bool> func)
         {
             var toRemoves = col.Where(func).ToList();
@@ -57,15 +56,15 @@ namespace azutil_core
             return col;
         }
         
-        public static IList<T> RemoveWhere<T>(this IList<T> col, Func<T, bool> func)
-        {
-            var toRemoves = col.Where(func).ToList();
-            foreach (var toRemove in toRemoves)
-            {
-                col.Remove(toRemove);
-            }
-            return col;
-        }
+        //public static IList<T> RemoveWhere<T>(this IList<T> col, Func<T, bool> func)
+        //{
+        //    var toRemoves = col.Where(func).ToList();
+        //    foreach (var toRemove in toRemoves)
+        //    {
+        //        col.Remove(toRemove);
+        //    }
+        //    return col;
+        //}
         
         
         public static async Task<string> GetCountyCodeByIp(this HttpClient httpClient, string ip = null)
